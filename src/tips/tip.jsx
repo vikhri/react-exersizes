8c306aac21af9e8c3cc8e127acbd938e64e2d6fc
vikhri <vikhrova.irina@gmail.com>
2023-07-06 18:27:08 +0300

    добавлен калькулятор чаевых

--- src/tips/tip.jsx
+++ src/tips/tip.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 
 function Tip () {
   return (
@@ -9,41 +10,55 @@ function Tip () {
 
 
 function Form () {
+  const [total, setTotal] = useState('');
+  const [myTips, setMyTips] = useState(0);
+  const [friendTips, setFriendTips] = useState(0);
+
+  let tipSum = Math.floor(((myTips + friendTips) / 2  * total) * 100) / 100;
+  let totalSum = total + tipSum;
+
+  function handleReset() {
+    setTotal('');
+    setFriendTips(0)
+    setMyTips(0)
+  }
+
   return (
-    <div style={{display: "flex", flexDirection: "column", rowGap: "20px", padding: '20px 0 60px'}}>
-      <Bill />
-      <Tips>How did you like?</Tips>
-      <Tips>How your friend likes?</Tips>
-      <p style={{fontSize: '24px', fontWeight: '700'}}>You pay $X ($Y + $Z tip)</p>
-      <Reset />
+    <div style={{display: "flex", flexDirection: "column", rowGap: "20px", padding: '20px 0 60px', width: '400px' }}>
+      <Bill onHandle={setTotal} sum={total}/>
+      <Tips onHandle={setMyTips} percentage={myTips}>How did you like?</Tips>
+      <Tips onHandle={setFriendTips} percentage={friendTips}>How your friend likes?</Tips>
+      <p style={{fontSize: '24px', fontWeight: '700'}}>You pay ${totalSum} (${total} + ${tipSum})</p>
+      <Reset onHandle={handleReset}/>
   </div>
   )}
 
-function Bill () {
+function Bill ({onHandle, sum}) {
   return (
-    <label>
+    <label style={{display: 'flex', justifyContent: 'space-between',}}>
       How much was the bill?
-      <input type='number'></input>
+      <input value={sum} style={{width: '70px'}} type='number' onChange={(e) => onHandle(Number(e.target.value))}/>
     </label>
   )
 }
 
-function Tips ({children}) {
+function Tips ({onHandle, percentage, children}) {
   return (
-    <label>{children}
-      <select>
-        <option value='0.1'>It was good (10%)</option>
+    <label style={{display: 'flex', justifyContent: 'space-between'}}>{children}
+      <select value={percentage} onChange={(e) => onHandle(parseFloat(e.target.value))}>
         <option value='0'>It was bad (0%)</option>
+        <option value='0.1'>It was good (10%)</option>
         <option value='0.2'>It was great (20%)</option>
       </select>
     </label>
   )
 }
 
-function Reset () {
+function Reset ({onHandle}) {
   return <button
     type='button'
     style={{width: '12em'}}
+    onClick={onHandle}
   >RESET</button>
 }
 
